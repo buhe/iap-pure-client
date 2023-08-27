f3a179cb7088458b4a5e92dd3d739ab5449aed23
buhe <bugu1986@126.com>
2023-08-27 14:16:38 +0800

    "Add IAPCache.swift file with a struct IAPCache that includes properties for cacheRetry, timeout, and cacheCount, and a method checkSubscriptionStatus that checks the subscription status and updates the cacheCount and timeout properties accordingly."

---
+++ Sources/IAP/IAPCache.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by 顾艳华 on 2023/8/27.
+//
+
+import Foundation
+
+public struct IAPCache {
+    public static var five = IAPCache()
+    
+    var cacheRetry: Int
+    public init(cacheRetry: Int = 5) {
+        self.cacheRetry = cacheRetry
+    }
+    private var timeout = true
+    private var cacheCount = 0
+    
+    public mutating func checkSubscriptionStatus(password: String) -> Bool {
+        if timeout && cacheCount < cacheRetry {
+            let result = IAPManager.shared.checkSubscriptionStatus(password: password)
+            cacheCount = 0
+            timeout = !result
+            
+            return result
+        } else {
+            cacheCount += 1
+            return true
+        }
+    }
+}
